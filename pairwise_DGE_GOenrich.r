2d6c147265f18a560352bf60e0ed0405333efafb
teak-log <115986812+teak-log@users.noreply.github.com>
2026-09-15 15:09:27 +0200

    Update pairwise_DGE_GOenrich.r

--- pairwise_DGE_GOenrich.r
+++ pairwise_DGE_GOenrich.r
@@ -1,3 +1,6 @@
+# The raw RNAseq bam file was preprocessed using featureCounts to generate the counts file that is used as input for the following R code:
+# featureCounts" "-p" "--countReadPairs" "-t" "exon" "-g" "gene_id" "-a" "braker.gtf" "--tmpDir" "/tmp/slurm-8220931" "-T" "4" "-o" "RNAcounts.txt" "RNAinput.bam" >
+
 library(DESeq2)
 library(edgeR)
 library(ggplot2)
@@ -11,10 +14,10 @@ library(ggtext)
 setwd("C:/Users/Admin/Documents/rna/root/")
 counts_raw <- read.csv("C:/Users/Admin/Documents/rna/root/rootRNAcounts_ordered", header = TRUE, sep = "\t")
 counts <- DGEList(counts = counts_raw[,c(1,7:42)], group =
-                    as.factor(rep(c("sed","ult","sed","ult","sed", "ult"), rep.int(6,6))))
-species <- as.factor(rep(c("cal","spn","imp","heq","lab","rev"), rep.int(6,6) ))
+                    as.factor(rep(c("sed","ult","sed","ult","sed","ult"), rep.int(6,6))))  # native soil-type assignments
+species <- as.factor(rep(c("cal","spn","imp","heq","lab","rev"), rep.int(6,6) ))  # species assignments
 counts$samples$species <- species
-group <- counts$samples$group     # counts dataframe specifying species and soil preferences
+group <- counts$samples$group  # counts dataframe specifying species and soil preferences
 
 annotation <- read.csv("C:/Users/Admin/Documents/rna/amin_redo/vie1167c_OmicsboxAminGene_Fannotation.txt", sep = "\t")
 annotation <- annotation[annotation$SeqName %in% counts$genes$Geneid,]
@@ -22,10 +25,10 @@ annotation <- annotation[,c("SeqName", "Description","Length","GO.Names","GO.IDs
 
 
 ### Filter & normalise ###
-dim(counts)	#check initial number of genes
+dim(counts)  #check initial number of genes
 keep.exprs <- filterByExpr(counts, group=species)
 counts <- counts[keep.exprs,, keep.lib.sizes=FALSE]  # 10/(median lib size) CPM cutoff met for at least 6 samples (size of a species group)
-dim(counts) #check final number of genes post-filtering
+dim(counts)  #check final number of genes post-filtering
 
 
 ### Set up comparisons and find DEGs ###
@@ -51,8 +54,8 @@ write.table(sig_revlab[,c(8,9,11,12,3,6,7,4,5,2,10)], file = "revlab_root_deseq_
 
 
 ### Setting up GO and gene universe ###
-# Annotation and gene list files
-args <- c("C:/Users/Admin/Documents/rna/amin_redo/topgo_amin.ann","revlab_root_deseq_Ppt05_lfc1pt5.genes")
+
+args <- c("C:/Users/Admin/Documents/rna/amin_redo/topgo_amin.ann","revlab_root_deseq_Ppt05_lfc1pt5.genes")  # Annotation and significant genes list
 universeFile <- args[1]
 interestingGenesFile <- args[2]
 
@@ -61,7 +64,6 @@ geneID2GO <- readMappings(file = universeFile)
 geneUniverse <- intersect(names(geneID2GO), counts[["genes"]]$Geneid)
 geneID2GO <- geneID2GO[geneUniverse]
 
-# Read significant genes list
 genesOfInterest <- read.table(interestingGenesFile, sep = "\t", header = TRUE, quote = "")
 genesOfInterest <- as.character(genesOfInterest$SeqName)
 
@@ -82,12 +84,10 @@ run_topgo_zscore <- function(ontology) {
   
   resultTopgo <- runTest(myGOdata, algorithm = "weight01", statistic = "fisher")
   
-  # Number of significant GO terms (p ≤ 0.05)
-  numsignif <- sum(attributes(resultTopgo)$score <= 0.05)
+  numsignif <- sum(attributes(resultTopgo)$score <= 0.05)  # Number of significant GO terms (p ≤ 0.05)
   
-  # Get topGO table
   GOresults <- GenTable(myGOdata, topgoFisher = resultTopgo,
-                        orderBy = "topgoFisher", topNodes = numsignif)
+                        orderBy = "topgoFisher", topNodes = numsignif)  # Get topGO table
   GOresults$logP <- -log10(as.numeric(GOresults$topgoFisher))
   
   # Calculate z-scores using topGO gene membership
@@ -108,26 +108,21 @@ run_topgo_zscore <- function(ontology) {
     )
   }) %>% bind_rows()
   
-  # Join results
-  GOresults <- left_join(GOresults, zscore_per_GO, by = c("GO.ID" = "GO.ID"))
-  
-  # Add ontology column for clarity
-  GOresults$Ontology <- ontology
-  
+  GOresults <- left_join(GOresults, zscore_per_GO, by = c("GO.ID" = "GO.ID"))  # Join results
+  GOresults$Ontology <- ontology  # Add ontology column for clarity
   return(GOresults)
 }
 
 
 ### Running GO enrichment for all three ontologies, combining results and plotting bar charts ###
-results_BP <- run_topgo_zscore("BP")
-results_CC <- run_topgo_zscore("CC")
-results_MF <- run_topgo_zscore("MF")
+results_BP <- run_topgo_zscore("BP")  # Biological Processes
+results_CC <- run_topgo_zscore("CC")  # Cellular Component
+results_MF <- run_topgo_zscore("MF")  # Molecular Function
 
-all_results <- bind_rows(results_BP, results_CC, results_MF)
+all_results <- bind_rows(results_BP, results_CC, results_MF)  # combine ontologies
 
-# Save combined results
 write.table(all_results, file = "revlab_root_deseq_padj05_lfc1pt5_zscore.txt",
-            sep = "\t", row.names = FALSE, quote = FALSE)
+            sep = "\t", row.names = FALSE, quote = FALSE)  # Save combined results
 
 # Plot bar charts where length of bar = -log(pval), continuous colour scales denote z-score,
 # and figures in the bars are of the form no. of DE genes / no. of tested genes in the GO term
